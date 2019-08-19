502a3d1f47c55b6cd57ce76850c20fd2b145d979
최현호 <gusgh9424@naver.com>
2019-08-19 20:41:11 +0900

    2019-08-19 baek_17406

--- baekjoon_py/17406.py
+++ baekjoon_py/17406.py
@@ -1,51 +1,34 @@
-def makeCombination(comb):
-    global K, combs
-    if len(comb) == K:
-        combs.append(comb)
+def makePermutation(perm):
+    global K, perms
+    if len(perm) == K:
+        perms.append(perm)
         return
     else:
         for num in range(K):
-            if not num in comb:
-                makeCombination(comb + [num])
+            if not num in perm:
+                makePermutation(perm + [num])
 
 
-def turnArrayRight(comb):
-    global board, turns
-    for num in comb:
+def turnArrayRight(perm):
+    global copied, turns
+    for num in perm:
         x, y, distance = turns[num]
-        x, y = x-1, y-1
         for d in range(1, distance+1):
-            tx = x - d
-            ty = y - d
-            temp = board[tx][ty]
+            x -= 1
+            y -= 1
+            temp = copied[x][y]
             for i in range(4):
                 for _ in range(2 * d):
-                    board[tx][ty] = board[tx + rx[i]][ty + ry[i]]
-                    tx, ty = tx + rx[i], ty + ry[i]
-            board[tx - rx[i]][ty - ry[i]] = temp
-
-
-def turnArrayLeft(comb):
-    global board, turns
-    for num in comb[::-1]:
-        x, y, distance = turns[num]
-        x, y = x-1, y-1
-        for d in range(1, distance+1):
-            tx = x - d
-            ty = y - d
-            temp = board[tx][ty]
-            for i in range(4):
-                for _ in range(2 * d):
-                    board[tx][ty] = board[tx + lx[i]][ty + ly[i]]
-                    tx, ty = tx + lx[i], ty + ly[i]
-            board[tx - lx[i]][ty - ly[i]] = temp
-        
+                    copied[x][y] = copied[x + rx[i]][y + ry[i]]
+                    x, y = x + rx[i], y + ry[i]
+            copied[x - rx[i]][y - ry[i]] = temp
+            
 
 def checkSum():
-    global board, M, result
+    global copied, M, result
     temp = 100 *  M
-    for bd in board:
-        tempSum = sum(bd)
+    for cp in copied:
+        tempSum = sum(cp)
         if temp > tempSum:
             temp = tempSum
     if result > temp:
@@ -55,9 +38,6 @@ def checkSum():
 rx = [1, 0, -1, 0]
 ry = [0, 1, 0, -1]
 
-lx = [0, 1, 0, -1]
-ly = [1, 0, -1, 0]
-
 
 N, M, K = map(int, input().split())
 board = [0] * N
@@ -65,12 +45,13 @@ for idx in range(N):
     board[idx] = list(map(int, input().split()))
 turns = [0] * K
 for idx in range(K):
-    turns[idx] = tuple(map(int, input().split()))
-combs = []
-makeCombination([])
+    x, y, d = map(int, input().split())
+    turns[idx] = (x-1, y-1, d)
+perms = []
+makePermutation([])
 result = 100 * M
-for comb in combs:
-    turnArrayRight(comb)
+for perm in perms:
+    copied = [bd[:] for bd in board]
+    turnArrayRight(perm)
     checkSum()
-    turnArrayLeft(comb)
 print(result)
